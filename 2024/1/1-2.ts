43e09f5f43d28dd5f58a281f785e1758f39ce059
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2024-12-01 13:19:20 -0600

    solve day 1 part 2

---
+++ 2024/1/1-2.ts
@@ -0,0 +1,25 @@
+// Split input into the two lists
+const items = Deno.readTextFileSync("input.txt").split("\n");
+const listA: number[] = [];
+const listB: number[] = [];
+items.forEach((string) => {
+  const [a, b] = string.split("   ");
+  listA.push(Number(a));
+  listB.push(Number(b));
+});
+
+// Count occurrences in list B
+const countB: Map<number, number> = new Map();
+listB.forEach((value) => {
+  const current = countB.get(value);
+  current ? countB.set(value, current + 1) : countB.set(value, 1);
+});
+
+// Calculate similarity score
+let similarityScore = 0;
+listA.forEach((value) => {
+  const reoccurrenceListB = countB.get(value);
+  similarityScore += reoccurrenceListB ? value * reoccurrenceListB : value * 0;
+});
+
+console.log(similarityScore);
