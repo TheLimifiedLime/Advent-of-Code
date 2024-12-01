c926f0747d18a2fe4f0992e2c159ccbf19eb212c
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2024-12-01 01:24:06 -0600

    solve day 1 part 1

---
+++ 2024/1/1-1.ts
@@ -0,0 +1,21 @@
+// Split input into the two lists
+const items = Deno.readTextFileSync("input.txt").split("\n");
+const listA: number[] = [];
+const listB: number[] = [];
+items.forEach((string) => {
+  const [a, b] = string.split("   ");
+  listA.push(Number(a));
+  listB.push(Number(b));
+});
+
+// Sort
+listA.sort((a, b) => a - b);
+listB.sort((a, b) => a - b);
+
+// Calculate distance
+let result = 0;
+for (let i = 0; i < items.length; i++) {
+  result += Math.abs(listA[i] - listB[i]);
+}
+
+console.log(result);
