49c4a188218e765905beb999b0b08e15db8c7067
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2024-12-03 20:58:53 -0600

    solve day 3 part 1

---
+++ 2024/3/3-1.ts
@@ -0,0 +1,13 @@
+const input = Deno.readTextFileSync("./input.txt");
+
+const operations = [...input.matchAll(/mul\((?<num1>\d+),(?<num2>\d+)\)/g)];
+
+const results: number[] = operations.map((op) => {
+  if (!op.groups) return 0;
+  const num1 = Number(op.groups.num1);
+  const num2 = Number(op.groups.num2);
+
+  return num1 * num2;
+});
+
+console.log(results.reduce((accumulator, value) => accumulator + value));
