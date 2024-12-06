f5da80b7c0b52fe65722ad6a7c9502814ff6fd8a
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2024-12-06 00:19:49 -0600

    solve day 5 part 1

---
+++ 2024/5/5-1.ts
@@ -0,0 +1,29 @@
+const [ruleInput, updateInput] =
+  Deno.readTextFileSync("./input.txt").split("\n\n");
+
+const rules = ruleInput
+  .split("\n")
+  .map((line) => line.split("|").map((number) => Number(number)));
+
+const updates = updateInput
+  .split("\n")
+  .map((line) => line.split(",").map((number) => Number(number)));
+
+const valid = updates.filter((update) => {
+  return !rules
+    .map(([first, second]) => {
+      const firstIndex = update.indexOf(first);
+      const secondIndex = update.indexOf(second);
+
+      if (firstIndex === -1 || secondIndex === -1) return true;
+
+      return firstIndex < secondIndex;
+    })
+    .includes(false);
+});
+
+const result = valid
+  .map((update) => update[Math.floor(update.length / 2)])
+  .reduce((accumulator, current) => accumulator + current);
+
+console.log(result);
