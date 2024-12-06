3763f8c84aacc3c926332743c5f7af8849bfbc66
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2024-12-05 20:39:52 -0600

    solve day 4 part 2

---
+++ 2024/4/4-2.ts
@@ -0,0 +1,37 @@
+const input = Deno.readTextFileSync("./input.txt")
+  .split("\n")
+  .map((line) => line.split(""));
+
+let count = 0;
+input.forEach((line, lineIndex) => {
+  line.forEach((_character, characterIndex) => {
+    const [x1, y1] = [1, 1]; // bottom right
+    const mas1 = [];
+    for (let charCount = 0; charCount < 3; charCount++) {
+      const char =
+        input[lineIndex + charCount * y1]?.[characterIndex + charCount * x1];
+      if (!char) return;
+      mas1.push(char);
+    }
+
+    const [x2, y2] = [-1, 1]; // bottom left
+    const mas2 = [];
+    for (let charCount = 0; charCount < 3; charCount++) {
+      const char =
+        input[lineIndex + charCount * y2]?.[
+          characterIndex + charCount * x2 + 2
+        ];
+      if (!char) return;
+      mas2.push(char);
+    }
+
+    if (
+      (mas1.join("").includes("MAS") || mas1.join("").includes("SAM")) &&
+      (mas2.join("").includes("MAS") || mas2.join("").includes("SAM"))
+    ) {
+      count++;
+    }
+  });
+});
+
+console.log(count);
