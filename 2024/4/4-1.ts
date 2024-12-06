136e0dcef4d99c99d24cf0a1de3d9b9c00fba0b8
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2024-12-05 19:44:07 -0600

    solve day 4 part 1

---
+++ 2024/4/4-1.ts
@@ -0,0 +1,31 @@
+const input = Deno.readTextFileSync("./input.txt")
+  .split("\n")
+  .map((line) => line.split(""));
+
+const directions = [
+  [-1, -1], // up left
+  [0, -1], // up
+  [1, -1], // up right
+  [-1, 0], // left
+  [1, 0], // right
+  [-1, 1], // bottom left
+  [0, 1], // bottom
+  [1, 1], // bottom right
+];
+let count = 0;
+input.forEach((line, lineIndex) => {
+  line.forEach((_character, characterIndex) => {
+    directions.forEach(([x, y]) => {
+      const foundChars = [];
+      for (let charCount = 0; charCount < 4; charCount++) {
+        const char =
+          input[lineIndex + charCount * y]?.[characterIndex + charCount * x];
+        if (!char) return;
+        foundChars.push(char);
+      }
+      if (foundChars.join("") === "XMAS") count++;
+    });
+  });
+});
+
+console.log(count);
